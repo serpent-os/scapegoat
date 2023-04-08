1c0ca1d09044f20705cdbd4432842da42a2b16b4
Ikey Doherty <ikey@serpentos.com>
2023-04-08 22:57:31 +0100

    Basic skeletal entry point
    Signed-off-by: Ikey Doherty <ikey@serpentos.com>

---
+++ source/app.d
@@ -0,0 +1,18 @@
+/*
+ * SPDX-FileCopyrightText: Copyright © 2020-2023 Serpent OS Developers
+ *
+ * SPDX-License-Identifier: Zlib
+ */
+
+/**
+ * Main entry point
+ *
+ * Authors: Copyright © 2020-2023 Serpent OS Developers
+ * License: Zlib
+ */
+module main;
+
+void main(string[] args) @safe
+{
+
+}
